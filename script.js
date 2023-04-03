52a8594f1a34fcef6f87aa8b3e6bbb341cacb7f1
Stefro <stefan.ro95@gmail.com>
2023-04-03 19:13:56 +0300

    Rename navbar element

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 window.onscroll = function () { myFunction() };
 
 // Get the navbar
-var header = document.getElementById("header");
+var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-        header.classList.add("sticky")
+        navbar.classList.add("sticky")
     } else {
-        header.classList.remove("sticky");
+        navbar.classList.remove("sticky");
     }
 }
