02d2348f35361bf9949e834d9ec65f51588824bb
PopescuStefan1 <stefan.ro95@gmail.com>
2023-04-30 15:52:24 +0300

    Style change

--- script.js
+++ script.js
@@ -1,71 +1,79 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () {
+  myFunction();
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 const headerLink = document.querySelector(".header > a");
-headerLink.onmouseover = function () { ballBounce() };
-headerLink.onmouseleave = function () { ballStopBounce() };
+headerLink.onmouseover = function () {
+  ballBounce();
+};
+headerLink.onmouseleave = function () {
+  ballStopBounce();
+};
 
 // Adds bounce on hover
 
 function ballBounce() {
-    setTimeout(function () {
-        balls = headerLink.children;
-        balls[0].classList.add("fa-bounce");
-        balls[1].classList.add("fa-bounce");
-    }, 150);
+  setTimeout(function () {
+    balls = headerLink.children;
+    balls[0].classList.add("fa-bounce");
+    balls[1].classList.add("fa-bounce");
+  }, 150);
 }
 function ballStopBounce() {
-    setTimeout(function () {
-        balls = headerLink.children;
-        balls[0].classList.remove("fa-bounce");
-        balls[1].classList.remove("fa-bounce");
-    }, 150)
+  setTimeout(function () {
+    balls = headerLink.children;
+    balls[0].classList.remove("fa-bounce");
+    balls[1].classList.remove("fa-bounce");
+  }, 150);
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 
-    scrollIndicator();
+  scrollIndicator();
 }
 
 // Fill and unfill scrollbar and parallax backgrounds
 function scrollIndicator() {
-    // Fill scrollbar
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById('scrollbar').style.width = scrolled + "%";
+  // Fill scrollbar
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollbar").style.width = scrolled + "%";
 
-    // Scroll parallax backgrounds
-    const parallaxes = document.querySelectorAll('.parallax');
-    for (const parallax of parallaxes) {
-        if (parallax.classList.contains('first-par')) {
-            parallax.style.backgroundPosition = `50% ${scrolled * 2}%`;
-        } else {
-            parallax.style.backgroundPosition = `50% ${scrolled}%`;
-        }
+  // Scroll parallax backgrounds
+  const parallaxes = document.querySelectorAll(".parallax");
+  for (const parallax of parallaxes) {
+    if (parallax.classList.contains("first-par")) {
+      parallax.style.backgroundPosition = `50% ${scrolled * 2}%`;
+    } else {
+      parallax.style.backgroundPosition = `50% ${scrolled}%`;
     }
+  }
 }
 
-const handleOnMouseMove = e => {
-    const { currentTarget: target } = e;
+const handleOnMouseMove = (e) => {
+  const { currentTarget: target } = e;
 
-    const rect = target.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+  const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
 
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
 
 for (const card of document.querySelectorAll(".card")) {
-    card.onmousemove = e => handleOnMouseMove(e);
+  card.onmousemove = (e) => handleOnMouseMove(e);
 }
