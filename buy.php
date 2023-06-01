3b67942c7978b815387c99a7292a6824336fb5f1
PopescuStefan1 <stefan.ro95@gmail.com>
2023-06-01 17:14:39 +0300

    Update seat availability in database on aquisition by user

--- buy.php
+++ buy.php
@@ -6,7 +6,16 @@ if (isset($_POST["submit"])) {
     // Prepared statement for seat status retrieval
     $sql = "SELECT * FROM `seats` WHERE `Row` = ? AND `Seat_No` = ?;";
     $stmt = mysqli_stmt_init($conn);
+
+    $sqlUpdate = "UPDATE `seats` SET `taken` = '1' WHERE `seats`.`Row` = ? AND `seats`.`Seat_No` = ?;";
+    $stmtUpdate = mysqli_stmt_init($conn);
+
     if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: matches.php?error=stmtfailed");
+        exit();
+    }
+
+    if (!mysqli_stmt_prepare($stmtUpdate, $sqlUpdate)) {
         header("location: matches.php?error=stmtfailed");
         exit();
     }
@@ -21,12 +30,17 @@ if (isset($_POST["submit"])) {
 
     if ($row = mysqli_fetch_assoc($resultData)) {
         if ($row["taken"] === 0) {
+            // Update seat after aquisition by user
+            mysqli_stmt_bind_param($stmtUpdate, "ii", $row, $seatNo);
+            mysqli_stmt_execute($stmtUpdate);
+
             header("location: matches.php?result=bought");
         } else {
             header("location: matches.php?result=error:seatTaken");
         }
     }
 
+    mysqli_stmt_close($stmtUpdate);
     mysqli_stmt_close($stmt);
     exit();
 } else {
