225c45c06ee6654765328f46ce7924b705a886e6
PopescuStefan1 <stefan.ro95@gmail.com>
2023-06-01 16:25:39 +0300

    Add ticket buying php

--- retrieve.php
+++ retrieve.php
@@ -1,14 +1,34 @@
 <?php
-// Prepared statement for seat status retrieval
-$sql = "SELECT * FROM `seats` WHERE `Row` = ? AND `Seat_No` = ?;";
-$stmt = mysqli_stmt_init($conn);
-if (!mysqli_stmt_prepare($stmt, $sql)) {
-    echo "SQL statement failed";
+
+require_once 'database.php';
+
+if (isset($_POST["submit"])) {
+    // Prepared statement for seat status retrieval
+    $sql = "SELECT * FROM `seats` WHERE `Row` = ? AND `Seat_No` = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: matches.html?error=stmtfailed");
+        exit();
+    }
+
+    $row = $_POST["rows"];
+    $seatNo = $_POST["seat"];
+
+    mysqli_stmt_bind_param($stmt, "ii", $row, $seatNo);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultData)) {
+        if ($row["taken"] === 0) {
+            header("location: matches.html?result=bought");
+        } else {
+            header("location: matches.html?result=error:seatTaken");
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+    exit();
 } else {
-
+    header("location: matches.html");
 }
-
-if ($row = mysqli_fetch_assoc($result)) {
-    echo $row['taken'];
-}
-?>
