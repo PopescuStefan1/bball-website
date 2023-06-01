c347cd3c04bfee9a0a06a84fad2353818443c6ae
PopescuStefan1 <stefan.ro95@gmail.com>
2023-06-01 14:53:07 +0300

    Change matches extension to allow php code

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "basketball_website";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
